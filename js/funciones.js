a4a1dcc31fead29a51b1e18e1177e511ac1ac979
René Garcia Luces <renegarcialuces@gmail.com>
2019-06-22 04:46:00 -0430

    Primera version del sitio ImuebleWeb

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+// Call carousel manually
+$('#myCarouselCustom').carousel();
+// Go to the previous item
+$("#prevBtn").click(function(){
+$("#myCarouselCustom").carousel("prev");
+});
+// Go to the previous item
+$("#nextBtn").click(function(){
+$("#myCarouselCustom").carousel("next");
+});
+</script>
